342262c8cca89a2a8aba5dc5c545e42c03578041
lrb37 <97558585+lrb37@users.noreply.github.com>
2023-03-23 16:09:30 -0500

    Update TSP.hpp

--- TSP.hpp
+++ TSP.hpp
@@ -3,19 +3,12 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <vector>
-#include <numeric>
-#include <cstdlib>
-#include <ctime>
-#include <utility>
-#include <algorithm>
-#include <limits>
 
 void readDistances(const std::string& filename, std::vector<std::vector<double>>& distances);
 double calculateCost(const std::vector<int>& path, const std::vector<std::vector<double>>& distances);
-
 std::vector<int> bruteForce(const std::vector<std::vector<double>>& distances, const int& numCities);
-
 std::vector<int> geneticAlgorithm(const std::vector<std::vector<double>>& distances, const int& numCities, const int& populationSize, const int& numGenerations, const double& mutationRate);
 
 #endif
