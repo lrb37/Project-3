16077a89a9df4850e659c6aa2619e1a63fdf602c
lrb37 <97558585+lrb37@users.noreply.github.com>
2023-03-23 01:47:33 -0500

    Update TSP.hpp

--- TSP.hpp
+++ TSP.hpp
@@ -6,9 +6,20 @@
 #include <string>
 #include <vector>
 
-void readDistances(const std::string& filename, std::vector<std::vector<double>>& distances);
-double calculateCost(const std::vector<int>& path, const std::vector<std::vector<double>>& distances);
-std::vector<int> bruteForce(const std::vector<std::vector<double>>& distances, const int& numCities);
-std::vector<int> geneticAlgorithm(const std::vector<std::vector<double>>& distances, const int& numCities, const int& populationSize, const int& numGenerations, const double& mutationRate);
+void readDistances (const std::string & filename,
+		     std::vector < std::vector < double >>&distances);
 
-#endif
+double calculateCost (const std::vector < int >&path,
+		       const std::vector < std::vector < double >>&distances);
+
+std::vector < int >
+bruteForce (const std::vector < std::vector < double >>&distances,
+	    const int &numCities);
+
+std::vector < int >
+geneticAlgorithm (const std::vector < std::vector < double >>&distances,
+		  const int &numCities, const int &populationSize,
+		  const int &numGenerations, const double &mutationRate);
+
+ 
+#endif // TSP_HPP
